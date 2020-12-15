ba1283e434f7a77f9ee3da84b0a206480ceda689
Katie Embrey-Farquhar <kembreyfarquhar@gmail.com>
2020-12-15 04:55:19 -0600

    removes strict mode

--- client/src/index.js
+++ client/src/index.js
@@ -8,9 +8,4 @@ import "./fonts/Lato-Semibold.ttf";
 import "./fonts/Lato-Thin.ttf";
 import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
